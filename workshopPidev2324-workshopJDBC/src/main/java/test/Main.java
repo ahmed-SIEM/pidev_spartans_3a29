b2e1853d710e234dd095ec5aad0ea34b3ee0419b
KabsiMontassar <kebsimontassar@gmail.com>
2024-02-17 14:11:06 +0100

    initial

---
+++ workshopPidev2324-workshopJDBC/src/main/java/test/Main.java
@@ -0,0 +1,21 @@
+package test;
+
+import models.User;
+import services.GestionUser.UserService;
+
+import java.sql.SQLException;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        UserService Us = new UserService();
+
+
+
+
+      
+
+
+    }
+}
