ba9a815c9eebef1d7a3cea70e6bfcf685699e580
KabsiMontassar <kebsimontassar@gmail.com>
2024-02-18 21:31:35 +0100

    Update Main.java

--- workshopPidev2324-workshopJDBC/src/main/java/test/Main.java
+++ workshopPidev2324-workshopJDBC/src/main/java/test/Main.java
@@ -13,6 +13,7 @@ public class Main {
         UserService Us = new UserService();
         User u1 = new User(25,"kebsi@hotmail.com","tunis","1234","kebsi",12345678,"joueur");
         User u2 = new User(20,"hamdi@hotmail.com","marsa","1234","hamdi",12345678,"propriétaire de terrain");
+        User u3 = new User(10,"hamdi@hotmail.com","marsa","1234","hamdi",12345678,"propriétaire de terrain");
 
 
 
